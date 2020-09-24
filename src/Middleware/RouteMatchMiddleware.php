bfab09211f04cc36e3c84a1dffe7c5df9f58f4c5
devanych <mail@devanych.ru>
2020-09-24 12:45:24 +0300

    Add RouteMatchMiddleware

---
+++ src/Middleware/RouteMatchMiddleware.php
@@ -0,0 +1,105 @@
+<?php
+
+declare(strict_types=1);
+
+namespace HttpSoft\Router\Middleware;
+
+use HttpSoft\Router\Route;
+use HttpSoft\Router\RouteCollector;
+use Psr\Http\Message\ResponseFactoryInterface;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Server\MiddlewareInterface;
+use Psr\Http\Server\RequestHandlerInterface;
+
+use function array_filter;
+use function array_unique;
+use function implode;
+use function in_array;
+use function is_string;
+use function strtoupper;
+
+final class RouteMatchMiddleware implements MiddlewareInterface
+{
+    /**
+     * @var RouteCollector
+     */
+    private RouteCollector $router;
+
+    /**
+     * @var ResponseFactoryInterface
+     */
+    private ResponseFactoryInterface $responseFactory;
+
+    /**
+     * @var array|string[]
+     */
+    private array $allowedMethods = [];
+
+    /**
+     * @param RouteCollector $router
+     * @param ResponseFactoryInterface $responseFactory
+     * @param array|string[] $allowedMethods common allowed request methods for all routes.
+     * @psalm-suppress MixedAssignment
+     */
+    public function __construct(
+        RouteCollector $router,
+        ResponseFactoryInterface $responseFactory,
+        array $allowedMethods = ['HEAD']
+    ) {
+        $this->router = $router;
+        $this->responseFactory = $responseFactory;
+
+        foreach ($allowedMethods as $allowedMethod) {
+            if (is_string($allowedMethod)) {
+                $this->allowedMethods[] = strtoupper($allowedMethod);
+            }
+        }
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function process(ServerRequestInterface $request, RequestHandlerInterface $handler): ResponseInterface
+    {
+        if (!$route = $this->router->routes()->match($request, false)) {
+            return $handler->handle($request);
+        }
+
+        if (!$this->isAllowedMethods($request->getMethod()) && !$route->isAllowedMethod($request->getMethod())) {
+            return $this->getEmptyResponseWithAllowedMethods($route->getMethods());
+        }
+
+        foreach ($route->getMatchedParameters() as $name => $value) {
+            $request = $request->withAttribute($name, $value);
+        }
+
+        return $handler->handle($request->withAttribute(Route::class, $route));
+    }
+
+    /**
+     * @param array $methods
+     * @return ResponseInterface
+     * @psalm-suppress MixedAssignment
+     */
+    private function getEmptyResponseWithAllowedMethods(array $methods): ResponseInterface
+    {
+        foreach ($this->allowedMethods as $method) {
+            if (is_string($method)) {
+                $methods[] = $method;
+            }
+        }
+
+        $methods = implode(', ', array_unique(array_filter($methods)));
+        return $this->responseFactory->createResponse(405)->withHeader('Allow', $methods);
+    }
+
+    /**
+     * @param string $method
+     * @return bool
+     */
+    private function isAllowedMethods(string $method): bool
+    {
+        return ($this->allowedMethods !== [] && in_array(strtoupper($method), $this->allowedMethods, true));
+    }
+}
---
+++ tests/Middleware/RouteMatchMiddlewareTest.php
@@ -0,0 +1,110 @@
+<?php
+
+declare(strict_types=1);
+
+namespace HttpSoft\Tests\Router\Middleware;
+
+use HttpSoft\Message\Response;
+use HttpSoft\Message\ResponseFactory;
+use HttpSoft\Message\ServerRequest;
+use HttpSoft\Message\Uri;
+use HttpSoft\Router\Middleware\RouteMatchMiddleware;
+use HttpSoft\Router\Route;
+use HttpSoft\Router\RouteCollector;
+use PHPUnit\Framework\TestCase;
+use Psr\Http\Message\ResponseFactoryInterface;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Server\RequestHandlerInterface;
+
+class RouteMatchMiddlewareTest extends TestCase
+{
+    /**
+     * @var callable
+     */
+    private $handler;
+
+    /**
+     * @var RouteCollector
+     */
+    private RouteCollector $router;
+
+    /**
+     * @var ServerRequest
+     */
+    private ServerRequest $request;
+
+    /**
+     * @var ResponseFactoryInterface
+     */
+    private ResponseFactoryInterface $responseFactory;
+
+    public function setUp(): void
+    {
+        $this->router = new RouteCollector();
+        $this->request = new ServerRequest();
+        $this->responseFactory = new ResponseFactory();
+        $this->handler = fn(): ResponseInterface => new Response(201);
+    }
+
+    public function testProcessSuccess(): void
+    {
+        $this->router->get('home', '/', $this->handler);
+        $middleware = new RouteMatchMiddleware($this->router, $this->responseFactory);
+        $response = $middleware->process($this->request->withUri(new Uri('/')), $this->requestHandler());
+        $this->assertSame(201, $response->getStatusCode());
+    }
+
+    public function testProcessFailure(): void
+    {
+        $this->router->post('home', '/', $this->handler);
+        $middleware = new RouteMatchMiddleware($this->router, $this->responseFactory);
+        $response = $middleware->process($this->request->withUri(new Uri('/not-exist')), $this->requestHandler());
+        $this->assertSame(404, $response->getStatusCode());
+    }
+
+    public function testProcessRouteMatchedButNotAllowedRequestMethod(): void
+    {
+        $this->router->post('home', '/', $this->handler);
+        $middleware = new RouteMatchMiddleware($this->router, $this->responseFactory);
+        $response = $middleware->process($this->request->withUri(new Uri('/')), $this->requestHandler());
+        $this->assertSame('POST, HEAD', $response->getHeaderLine('Allow'));
+        $this->assertSame(405, $response->getStatusCode());
+    }
+
+    public function testProcessSuccessWithAllowedMethods(): void
+    {
+        $this->router->post('home', '/', $this->handler);
+        $middleware = new RouteMatchMiddleware($this->router, $this->responseFactory, ['GET']);
+        $response = $middleware->process($this->request->withUri(new Uri('/')), $this->requestHandler());
+        $this->assertSame(201, $response->getStatusCode());
+    }
+
+    public function testProcessSuccessWithAllowedMethodHeadByDefault(): void
+    {
+        $this->router->get('home', '/', $this->handler);
+        $middleware = new RouteMatchMiddleware($this->router, $this->responseFactory);
+        $response = $middleware->process(
+            $this->request->withMethod('HEAD')->withUri(new Uri('/')),
+            $this->requestHandler()
+        );
+        $this->assertSame(201, $response->getStatusCode());
+    }
+
+    /**
+     * @return RequestHandlerInterface
+     */
+    private function requestHandler(): RequestHandlerInterface
+    {
+        return new class implements RequestHandlerInterface {
+            public function handle(ServerRequestInterface $request): ResponseInterface
+            {
+                if ($route = $request->getAttribute(Route::class)) {
+                    return ($route->getHandler())();
+                }
+
+                return new Response(404);
+            }
+        };
+    }
+}
